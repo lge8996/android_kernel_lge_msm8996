6ccd41aa6b0cfacff49d178b91fa9f4dc2853054
Alejandro <apamarin@gmail.com>
2020-08-25 18:08:52 +0200

    Enable ccache in build_script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/astrako/android/kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE="ccache /media/astrako/DATOS/toolchain/gcc-linaro-4.9.4/bin/aarch64-linux-gnu-"
 export ANDROID_MAJOR_VERSION=o
+export ANDROID_PLATFORM_VERSION=9
 
 make O=./out $1
 make O=./out -j64
